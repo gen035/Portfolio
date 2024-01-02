cbffdb4b1078adbe34db10e2e0abf4423a324dd7
Geneviève Perron Migneron <info@gen-migneron.com>
2024-01-02 14:58:17 -0500

    Dynamically load skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,18 +1,31 @@
 import React, { FC } from 'react';
-import { FaHtml5, FaCss3Alt, FaSass, FaJsSquare, FaReact, FaAngular, FaVuejs } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaSass, FaJsSquare, FaReact, FaAngular, FaVuejs} from 'react-icons/fa';
 
-const Skills: FC = () => {
-  return (
-    <ul className="intro-content-skills my-2">
-      <li className="intro-content-skills-html"><FaHtml5 /></li>
-      <li className="intro-content-skills-css"><FaCss3Alt /></li>
-      <li className="intro-content-skills-sass"><FaSass /></li>
-      <li className="intro-content-skills-js"><FaJsSquare /></li>
-      <li className="intro-content-skills-react"><FaReact /></li>
-      <li className="intro-content-skills-angular"><FaAngular /></li>
-      <li className="intro-content-skills-vue"><FaVuejs /></li>
-    </ul>
-  );
+interface Skill {
+  name: string;
+  icon: React.ReactElement;
 }
 
-export default Skills;
+const skillsData: Skill[] = [
+  { name: 'HTML', icon: <FaHtml5 /> },
+  { name: 'CSS', icon: <FaCss3Alt /> },
+  { name: 'Sass', icon: <FaSass /> },
+  { name: 'JavaScript', icon: <FaJsSquare /> },
+  { name: 'React', icon: <FaReact /> },
+  { name: 'Angular', icon: <FaAngular /> },
+  { name: 'Vue.js', icon: <FaVuejs /> },
+];
+
+const SkillsList: FC = () => {
+  return (
+    <ul className="intro-content-skills my-2">
+      {skillsData.map((skill, index) => (
+        <li key={index} className={`intro-content-skills-${skill.name.toLowerCase()}`}>
+          {skill.icon}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SkillsList;
