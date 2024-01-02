5309db1173ac305cb39a33fbd6671befd4bc0cb2
Geneviève Perron Migneron <info@gen-migneron.com>
2024-01-02 16:40:17 -0500

    add OG

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang}>
+      <meta property="og:title" content="Genevieve Perron Migneron - Portfolio" />
+      <meta property="og:description" content="Senior Manager - Web Development" />
+      <meta property="og:image" content="https://www.gen-migneron.com/og.jpg" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Genevieve Perron Migneron - Portfolio" />
+      <meta name="twitter:description" content="Senior Manager - Web Development" />
+      <meta name="twitter:image" content="https://www.gen-migneron.com/og.jpg" />
       <body className="flex items-center justify-center h-screen font-sans">
         <BackgroundImages />
         <SocialMediaLinks />
